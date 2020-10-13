49eeef32606eb81c78975312492fb29bed844fa3
Dan Lorenc <dlorenc@google.com>
2020-10-13 14:32:52 -0500

    Fix build error.

--- checks/all_checks.go
+++ checks/all_checks.go
@@ -4,7 +4,7 @@ import "github.com/dlorenc/scorecard/checker"
 
 var AllChecks = []checker.NamedCheck{}
 
-func registerCheck(name string, fn CheckFn) {
+func registerCheck(name string, fn checker.CheckFn) {
 	AllChecks = append(AllChecks, checker.NamedCheck{
 		Name: name,
 		Fn:   fn,
