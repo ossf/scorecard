04c9b3798107c6a8d677d19656668ec20b6fa195
Abhishek Arya <inferno@chromium.org>
2020-10-16 16:11:07 -0700

    Fix PR calc logic, remove bot accounts. (#17)
    * Fix PR calc logic, remove bot accounts.
    
    * Fmt.

--- checks/pull_requests.go
+++ checks/pull_requests.go
@@ -1,6 +1,8 @@
 package checks
 
 import (
+	"strings"
+
 	"github.com/dlorenc/scorecard/checker"
 	"github.com/google/go-github/v32/github"
 )
@@ -18,14 +20,28 @@ func PullRequests(c checker.Checker) checker.CheckResult {
 	total := 0
 	totalWithPrs := 0
 	for _, commit := range commits {
+		isBot := false
+		committer := commit.GetCommitter().GetLogin()
+		for _, substring := range []string{"bot", "gardener"} {
+			if strings.Contains(committer, substring) {
+				isBot = true
+				break
+			}
+		}
+		if isBot {
+			c.Logf("skip commit from bot account: %s", committer)
+			continue
+		}
+
 		prs, _, err := c.Client.PullRequests.ListPullRequestsWithCommit(c.Ctx, c.Owner, c.Repo, commit.GetSHA(), &github.PullRequestListOptions{})
 		if err != nil {
 			return checker.RetryResult(err)
 		}
 		total++
 		if len(prs) > 0 {
+			c.Logf("found PRs, example: #%v", prs[0].GetNumber())
 			totalWithPrs++
 		}
 	}
-	return checker.ProportionalResult(totalWithPrs, total, .9)
+	return checker.ProportionalResult(totalWithPrs, total, .75)
 }
