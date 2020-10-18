49fba38c8b154968d5cdd9e796fa7d557c1a05dc
dlorenc <lorenc.d@gmail.com>
2020-10-18 18:49:51 -0500

    Use Cobra CLI library, reorganize a bit. (#22)

---
+++ cmd/root.go
@@ -0,0 +1,157 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+
+	goflag "flag"
+
+	"github.com/dlorenc/scorecard/checker"
+	"github.com/dlorenc/scorecard/checks"
+	"github.com/dlorenc/scorecard/roundtripper"
+	"github.com/google/go-github/v32/github"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var (
+	repo        string
+	checksToRun string
+	// This one has to use goflag instead of pflag because it's defined by zap
+	logLevel = zap.LevelFlag("verbosity", zap.InfoLevel, "override the default log level")
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long:  `A scorecard program!`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := zap.NewProductionConfig()
+		cfg.Level.SetLevel(*logLevel)
+		logger, _ := cfg.Build()
+		defer logger.Sync() // flushes buffer, if any
+		sugar := logger.Sugar()
+
+		split := strings.SplitN(repo, "/", 3)
+		if len(split) != 3 {
+			log.Fatalf("invalid repo flag: %s, pass the full repository URL", repo)
+		}
+		host, owner, repo := split[0], split[1], split[2]
+
+		switch host {
+		case "github.com":
+		default:
+			log.Fatalf("unsupported host: %s", host)
+		}
+
+		ctx := context.Background()
+
+		// Use our custom roundtripper
+		rt := roundtripper.NewTransport(ctx, sugar)
+
+		client := &http.Client{
+			Transport: rt,
+		}
+		ghClient := github.NewClient(client)
+
+		c := checker.Checker{
+			Ctx:        ctx,
+			Client:     ghClient,
+			HttpClient: client,
+			Owner:      owner,
+			Repo:       repo,
+		}
+
+		resultsCh := make(chan result)
+		wg := sync.WaitGroup{}
+		checksToRunList := []string{}
+		if len(checksToRun) > 0 {
+			checksToRunList = strings.Split(checksToRun, ",")
+		}
+		for _, check := range checks.AllChecks {
+			check := check
+			if !stringInListOrEmpty(check.Name, checksToRunList) {
+				continue
+			}
+			wg.Add(1)
+			fmt.Fprintf(os.Stderr, "Starting [%s]\n", check.Name)
+			go func() {
+				defer wg.Done()
+				runner := checker.Runner{Checker: c}
+				r := runner.Run(check.Fn)
+				resultsCh <- result{
+					name: check.Name,
+					cr:   r,
+				}
+			}()
+		}
+		go func() {
+			wg.Wait()
+			close(resultsCh)
+		}()
+
+		// Collect results
+		results := []result{}
+		for result := range resultsCh {
+			fmt.Fprintf(os.Stderr, "Finished [%s]\n", result.name)
+			results = append(results, result)
+		}
+
+		// Sort them by name
+		sort.Slice(results, func(i, j int) bool {
+			return results[i].name < results[j].name
+		})
+
+		fmt.Println()
+		fmt.Println("RESULTS")
+		fmt.Println("-------")
+		for _, r := range results {
+			fmt.Println(r.name, displayResult(r.cr.Pass), r.cr.Confidence)
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+type result struct {
+	cr   checker.CheckResult
+	name string
+}
+
+func stringInListOrEmpty(s string, list []string) bool {
+	if len(list) == 0 {
+		return true
+	}
+	for _, le := range list {
+		if le == s {
+			return true
+		}
+	}
+	return false
+}
+
+func displayResult(result bool) string {
+	if result {
+		return "pass"
+	} else {
+		return "fail"
+	}
+}
+
+func init() {
+	// Add the zap flag manually
+	rootCmd.PersistentFlags().AddGoFlagSet(goflag.CommandLine)
+	rootCmd.PersistentFlags().StringVar(&repo, "repo", "", "repository to check")
+	rootCmd.PersistentFlags().StringVar(&checksToRun, "checks", "", "specific checks to run, instead of all")
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/dlorenc/scorecard
 go 1.15
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v32 v32.1.0
 	github.com/prometheus/common v0.14.0
+	github.com/spf13/cobra v1.1.0
+	github.com/spf13/viper v1.7.0
 	go.uber.org/zap v1.16.0
-	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 )
