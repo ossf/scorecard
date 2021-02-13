bca9ffb6f4a1a2de8cb40ade12bf1c95d3760e87
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-02-13 18:25:59 -0500

    Fix - Organization checks for members

--- checks/contributors.go
+++ checks/contributors.go
@@ -15,6 +15,8 @@
 package checks
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/google/go-github/v32/github"
@@ -26,6 +28,9 @@ func init() {
 }
 
 func Contributors(c checker.Checker) checker.CheckResult {
+	type orgnaizations []struct {
+		Login string `json:"login"`
+	}
 	contribs, _, err := c.Client.Repositories.ListContributors(c.Ctx, c.Owner, c.Repo, &github.ListContributorsOptions{})
 	if err != nil {
 		return checker.RetryResult(err)
@@ -38,6 +43,28 @@ func Contributors(c checker.Checker) checker.CheckResult {
 			if err != nil {
 				return checker.RetryResult(err)
 			}
+			// TODO - Figure out the real API for getting user orgs
+			url := fmt.Sprintf("https://api.github.com/users/%s/orgs", contrib.GetLogin())
+			resp, e := c.HttpClient.Get(url)
+
+			if e != nil {
+				c.Logf("unable to get org members for %s", contrib.GetLogin())
+			} else {
+				var orgs orgnaizations
+
+				err = json.NewDecoder(resp.Body).Decode(&orgs)
+
+				if err != nil {
+					c.Logf("unable to get  decode json for for %s org", contrib.GetLogin())
+				} else {
+					if len(orgs) > 0 {
+						companies[orgs[0].Login] = struct{}{}
+						continue
+					}
+				}
+			}
+
+			defer resp.Body.Close()
 			company := u.GetCompany()
 			if company != "" {
 				company = strings.ToLower(company)
