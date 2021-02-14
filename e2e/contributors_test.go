30d69310c68b8e66bab90dd8a3474e3de39ebc55
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-02-14 10:46:14 -0500

    Fix - Organization checks for members (#170)
    * Fix - Organization checks for members
    
    * Fix - Turn off automatic releasenotes generation
    
    Turn off automatic release notes for CII https://bestpractices.coreinfrastructure.org/
    
    * Fix - Organization checks for members

--- e2e/contributors_test.go
+++ e2e/contributors_test.go
@@ -17,6 +17,21 @@ var _ = Describe("E2E TEST:CodeReview", func() {
 				Ctx:         context.Background(),
 				Client:      ghClient,
 				HttpClient:  client,
+				Owner:       "ossf",
+				Repo:        "scorecard",
+				GraphClient: graphClient,
+				Logf:        l.Logf,
+			}
+			result := checks.Contributors(checker)
+			Expect(result.Error).Should(BeNil())
+			Expect(result.Pass).Should(BeTrue())
+		})
+		It("Should return valid project contributors", func() {
+			l := log{}
+			checker := checker.Checker{
+				Ctx:         context.Background(),
+				Client:      ghClient,
+				HttpClient:  client,
 				Owner:       "apache",
 				Repo:        "airflow",
 				GraphClient: graphClient,
