35fece6491395360d4e8ca22bc27b72965e20f99
Chris McGehee <cmcgehee@datto.com>
2021-05-22 10:29:18 -0700

    Fix lint issues: lll linter (#486)

--- checks/security_policy.go
+++ checks/security_policy.go
@@ -29,13 +29,14 @@ func init() {
 
 func SecurityPolicy(c *checker.CheckRequest) checker.CheckResult {
 	// check repository for repository-specific policy
-	result := CheckIfFileExists(securityPolicyStr, c, func(name string, logf func(s string, f ...interface{})) (bool, error) {
+	onFile := func(name string, logf func(s string, f ...interface{})) (bool, error) {
 		if strings.EqualFold(name, "security.md") {
 			logf("security policy : %s", name)
 			return true, nil
 		}
 		return false, nil
-	})
+	}
+	result := CheckIfFileExists(securityPolicyStr, c, onFile)
 
 	if result.Pass {
 		return result
@@ -46,11 +47,12 @@ func SecurityPolicy(c *checker.CheckRequest) checker.CheckResult {
 	dotGitHub := c
 	dotGitHub.Repo = ".github"
 
-	return CheckIfFileExists(securityPolicyStr, dotGitHub, func(name string, logf func(s string, f ...interface{})) (bool, error) {
+	onFile = func(name string, logf func(s string, f ...interface{})) (bool, error) {
 		if strings.EqualFold(name, "security.md") {
 			logf("security policy within .github folder : %s", name)
 			return true, nil
 		}
 		return false, nil
-	})
+	}
+	return CheckIfFileExists(securityPolicyStr, dotGitHub, onFile)
 }
