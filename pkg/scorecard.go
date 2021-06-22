7f1a4e5f247db86159254f3915bcfce5112c702a
laurentsimon <laurentsimon@google.com>
2021-06-22 01:43:57 +0000

    linter

--- pkg/scorecard.go
+++ pkg/scorecard.go
@@ -85,6 +85,7 @@ func RunScorecards(ctx context.Context,
 	if err := repoClient.InitRepo(repo.Owner, repo.Repo); err != nil {
 		return repos.RepoResult{}, fmt.Errorf("error during InitRepo for %s: %w", repo.URL(), err)
 	}
+	//nolint:errcheck
 	defer repoClient.ReleaseRepo()
 
 	ret := repos.RepoResult{
