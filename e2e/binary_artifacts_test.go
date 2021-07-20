74a9d73aac51a0280bde5a13ce114a363de62f75
laurentsimon <laurentsimon@google.com>
2021-07-20 18:30:13 +0000

    linter

--- e2e/binary_artifacts_test.go
+++ e2e/binary_artifacts_test.go
@@ -24,10 +24,6 @@ import (
 	"github.com/ossf/scorecard/checker"
 	"github.com/ossf/scorecard/checks"
 	"github.com/ossf/scorecard/clients/githubrepo"
-<<<<<<< HEAD
-=======
-
->>>>>>> 08e121c (missing errors.go)
 	scut "github.com/ossf/scorecard/utests"
 )
 
