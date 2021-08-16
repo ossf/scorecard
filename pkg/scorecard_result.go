05790fd60dff0926f4316886117e31885c7ca4fd
laurentsimon <laurentsimon@google.com>
2021-08-16 21:55:55 +0000

    typo

--- pkg/scorecard_result.go
+++ pkg/scorecard_result.go
@@ -106,7 +106,7 @@ func (r *ScorecardResult) AsJSON2(showDetails bool, logLevel zapcore.Level, writ
 		}
 		if showDetails {
 			for _, d := range checkResult.Details2 {
-				tmpResult.Details = append(tmpResult.Details, d.Msg.Text)
+				tmpResult.Details = append(tmpResult.Details, d.Msg)
 			}
 		}
 		out.Checks = append(out.Checks, tmpResult)
