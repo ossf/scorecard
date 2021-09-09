1da121da295cccce22dc4d7857d64203b3c97d84
Nanik <nanikjava@gmail.com>
2021-09-10 05:16:31 +1000

    :sparkles: Give low importance to github-owned actions (#802) (#906)
    * Different calculation between github and non-github actions
    
    * Add test case for different kind of github and non-github action
    
    * Modify existing test as score calculation has changed

--- checks/pinned_dependencies_test.go
+++ checks/pinned_dependencies_test.go
@@ -42,7 +42,7 @@ func TestGithubWorkflowPinning(t *testing.T) {
 				Error:         nil,
 				Score:         checker.MaxResultScore,
 				NumberOfWarn:  0,
-				NumberOfInfo:  1,
+				NumberOfInfo:  2,
 				NumberOfDebug: 0,
 			},
 		},
@@ -53,7 +53,7 @@ func TestGithubWorkflowPinning(t *testing.T) {
 				Error:         nil,
 				Score:         checker.MaxResultScore,
 				NumberOfWarn:  0,
-				NumberOfInfo:  1,
+				NumberOfInfo:  2,
 				NumberOfDebug: 0,
 			},
 		},
@@ -64,7 +64,7 @@ func TestGithubWorkflowPinning(t *testing.T) {
 				Error:         nil,
 				Score:         checker.MaxResultScore,
 				NumberOfWarn:  0,
-				NumberOfInfo:  1,
+				NumberOfInfo:  2,
 				NumberOfDebug: 0,
 			},
 		},
@@ -73,9 +73,98 @@ func TestGithubWorkflowPinning(t *testing.T) {
 			filename: "./testdata/workflow-not-pinned.yaml",
 			expected: scut.TestReturn{
 				Error:         nil,
-				Score:         checker.MinResultScore,
+				Score:         checker.MaxResultScore - 2,
 				NumberOfWarn:  1,
+				NumberOfInfo:  1,
+				NumberOfDebug: 0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt // Re-initializing variable so it is not changed while executing the closure below
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var content []byte
+			var err error
+
+			content, err = ioutil.ReadFile(tt.filename)
+			if err != nil {
+				panic(fmt.Errorf("cannot read file: %w", err))
+			}
+
+			dl := scut.TestDetailLogger{}
+			s, e := testIsGitHubActionsWorkflowPinned(tt.filename, content, &dl)
+			actual := checker.CheckResult{
+				Score:  s,
+				Error2: e,
+			}
+			if !scut.ValidateTestReturn(t, tt.name, &tt.expected, &actual, &dl) {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestNonGithubWorkflowPinning(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		filename string
+		expected scut.TestReturn
+	}{
+		{
+			name:     "Pinned non-github workflow",
+			filename: "./testdata/workflow-non-github-pinned.yaml",
+			expected: scut.TestReturn{
+				Error:         nil,
+				Score:         checker.MaxResultScore,
+				NumberOfWarn:  0,
+				NumberOfInfo:  2,
+				NumberOfDebug: 0,
+			},
+		},
+		{
+			name:     "Pinned github workflow",
+			filename: "./testdata/workflow-mix-github-and-non-github-not-pinned.yaml",
+			expected: scut.TestReturn{
+				Error:         nil,
+				Score:         checker.MinResultScore,
+				NumberOfWarn:  2,
 				NumberOfInfo:  0,
+				NumberOfDebug: 0,
+			},
+		},
+		{
+			name:     "Pinned github workflow",
+			filename: "./testdata/workflow-mix-github-and-non-github-pinned.yaml",
+			expected: scut.TestReturn{
+				Error:         nil,
+				Score:         checker.MaxResultScore,
+				NumberOfWarn:  0,
+				NumberOfInfo:  2,
+				NumberOfDebug: 0,
+			},
+		},
+		{
+			name:     "Mix of pinned and non-pinned GitHub actions",
+			filename: "./testdata/workflow-mix-pinned-and-non-pinned-github.yaml",
+			expected: scut.TestReturn{
+				Error:         nil,
+				Score:         checker.MaxResultScore - 2,
+				NumberOfWarn:  1,
+				NumberOfInfo:  1,
+				NumberOfDebug: 0,
+			},
+		},
+		{
+			name:     "Mix of pinned and non-pinned non-GitHub actions",
+			filename: "./testdata/workflow-mix-pinned-and-non-pinned-non-github.yaml",
+			expected: scut.TestReturn{
+				Error:         nil,
+				Score:         checker.MaxResultScore - 8,
+				NumberOfWarn:  1,
+				NumberOfInfo:  1,
 				NumberOfDebug: 0,
 			},
 		},
@@ -627,7 +716,7 @@ func TestGitHubWorkflowUsesLineNumber(t *testing.T) {
 				t.Errorf("cannot read file: %w", err)
 			}
 			dl := scut.TestDetailLogger{}
-			var pinned pinnedResult
+			var pinned worklowPinningResult
 			_, err = validateGitHubActionWorkflow(tt.filename, content, &dl, &pinned)
 			if err != nil {
 				t.Errorf("error during validateGitHubActionWorkflow: %w", err)
