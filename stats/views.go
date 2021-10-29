138552848d7c7ec2fd5885a3d38c8a30637b06d5
Azeem Shaikh <azeemshaikh38@gmail.com>
2021-10-29 00:37:44 -0400

    Remove Repo CPU runtime stat logging (#1186)
    Co-authored-by: Azeem Shaikh <azeems@google.com>

--- stats/views.go
+++ stats/views.go
@@ -55,31 +55,6 @@ var (
 		Aggregation: view.Count(),
 	}
 
-	// RepoRuntime tracks CPU runtime stats for repos.
-	RepoRuntime = view.View{
-		Name:        "RepoRuntime",
-		Description: "CPU runtime stats per repo",
-		Measure:     RepoRuntimeInSec,
-		TagKeys:     []tag.Key{Repo},
-		//nolint:gomnd
-		Aggregation: view.Distribution(
-			0,
-			1<<2,
-			1<<3,
-			1<<4,
-			1<<5,
-			1<<6,
-			1<<7,
-			1<<8,
-			1<<9,
-			1<<10,
-			1<<11,
-			1<<12,
-			1<<13,
-			1<<14,
-			1<<15),
-	}
-
 	// OutgoingHTTPRequests tracks HTTPRequests made.
 	OutgoingHTTPRequests = view.View{
 		Name:        "OutgoingHTTPRequests",
