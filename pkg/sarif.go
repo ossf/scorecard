b092c6afb9f8ab11aa54a18c507d6996c0613145
laurentsimon <laurentsimon@google.com>
2021-12-23 17:59:58 +0000

    fix

--- pkg/sarif.go
+++ pkg/sarif.go
@@ -501,7 +501,7 @@ func createCheckIdentifiers(name string) (string, string) {
 	return name, fmt.Sprintf("%sID", n)
 }
 
-func removeDetailType(details []checker.CheckDetail, t checker.DetailType) []checker.CheckDetail {
+func filterOutDetailType(details []checker.CheckDetail, t checker.DetailType) []checker.CheckDetail {
 	ret := make([]checker.CheckDetail, 0)
 	for i := range details {
 		d := details[i]
@@ -514,7 +514,7 @@ func removeDetailType(details []checker.CheckDetail, t checker.DetailType) []che
 }
 
 func createDefaultLocationMessage(check *checker.CheckResult) string {
-	details := removeDetailType(check.Details2, checker.DetailInfo)
+	details := filterOutDetailType(check.Details2, checker.DetailInfo)
 	s, b := detailsToString(details, zapcore.WarnLevel)
 	if b {
 		// Warning: GitHub UX needs a single `\n` to turn it into a `<br>`.
@@ -598,7 +598,6 @@ func (r *ScorecardResult) AsSARIF(showDetails bool, logLevel zapcore.Level,
 		RuleIndex := len(run.Tool.Driver.Rules) - 1
 		if len(locs) == 0 {
 			locs = addDefaultLocation(locs, "no file available")
-			// Use the `reason` as message, and append the detailed to it.
 			msg := createDefaultLocationMessage(&check)
 			cr := createSARIFCheckResult(RuleIndex, sarifCheckID, msg, &locs[0])
 			run.Results = append(run.Results, cr)
