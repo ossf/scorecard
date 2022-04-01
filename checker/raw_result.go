27dbf9c7e51e6b0f049854f0bdc8a53688807a81
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-01 16:13:58 -0700

    ✨ Raw results for Signed-Release check (#1789)
    * Raw results for Signed-Releases
    
    * updates
    
    * linter

--- checker/raw_result.go
+++ checker/raw_result.go
@@ -18,6 +18,7 @@ import "time"
 
 // RawResults contains results before a policy
 // is applied.
+//nolint
 type RawResults struct {
 	VulnerabilitiesResults      VulnerabilitiesData
 	BinaryArtifactResults       BinaryArtifactData
@@ -27,6 +28,7 @@ type RawResults struct {
 	CodeReviewResults           CodeReviewData
 	WebhookResults              WebhooksData
 	MaintainedResults           MaintainedData
+	SignedReleasesResults       SignedReleasesData
 }
 
 // MaintainedData contains the raw results
@@ -61,6 +63,12 @@ type SecurityPolicyData struct {
 type BinaryArtifactData struct {
 	// Files contains a list of files.
 	Files []File
+}
+
+// SignedReleasesData contains the raw results
+// for the Signed-Releases check.
+type SignedReleasesData struct {
+	Releases []Release
 }
 
 // DependencyUpdateToolData contains the raw results
@@ -226,4 +234,18 @@ type Vulnerability struct {
 	// For CVE: CVE-2022-23945
 	ID string
 	// TODO(vuln): Add additional fields, if needed.
+}
+
+// Release represents a project release.
+type Release struct {
+	Tag    string
+	URL    string
+	Assets []ReleaseAsset
+	// TODO: add needed fields, e.g. Path.
+}
+
+// ReleaseAsset represents a release asset.
+type ReleaseAsset struct {
+	Name string
+	URL  string
 }
