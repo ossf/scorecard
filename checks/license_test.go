4d1c5316907e496cb98bcb397c17b994f5c4f296
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-13 18:20:05 -0700

    ✨ Raw results for license (#1790)
    * Raw results for license
    
    * tests
    
    * tests
    
    * e2e fix
    
    * comment
    
    * fix
    
    * linter

--- checks/license_test.go
+++ checks/license_test.go
@@ -28,86 +28,6 @@ import (
 	scut "github.com/ossf/scorecard/v4/utests"
 )
 
-func TestLicenseFileCheck(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name     string
-		filename string
-	}{
-		{
-			name:     "LICENSE.md",
-			filename: "LICENSE.md",
-		},
-		{
-			name:     "LICENSE",
-			filename: "LICENSE",
-		},
-		{
-			name:     "COPYING",
-			filename: "COPYING",
-		},
-		{
-			name:     "COPYING.md",
-			filename: "COPYING.md",
-		},
-		{
-			name:     "LICENSE.textile",
-			filename: "LICENSE.textile",
-		},
-		{
-			name:     "COPYING.textile",
-			filename: "COPYING.textile",
-		},
-		{
-			name:     "LICENSE-MIT",
-			filename: "LICENSE-MIT",
-		},
-		{
-			name:     "COPYING-MIT",
-			filename: "COPYING-MIT",
-		},
-		{
-			name:     "MIT-LICENSE-MIT",
-			filename: "MIT-LICENSE-MIT",
-		},
-		{
-			name:     "MIT-COPYING",
-			filename: "MIT-COPYING",
-		},
-		{
-			name:     "OFL.md",
-			filename: "OFL.md",
-		},
-		{
-			name:     "OFL.textile",
-			filename: "OFL.textile",
-		},
-		{
-			name:     "OFL",
-			filename: "OFL",
-		},
-		{
-			name:     "PATENTS",
-			filename: "PATENTS",
-		},
-		{
-			name:     "PATENTS.txt",
-			filename: "PATENTS.txt",
-		},
-	}
-	for _, tt := range tests {
-		tt := tt // Re-initializing variable so it is not changed while executing the closure below
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			s := testLicenseCheck(tt.filename)
-			if !s {
-				t.Fail()
-			}
-		})
-	}
-}
-
 func TestLicenseFileSubdirectory(t *testing.T) {
 	t.Parallel()
 
@@ -167,7 +87,7 @@ func TestLicenseFileSubdirectory(t *testing.T) {
 				Dlogger:    &dl,
 			}
 
-			res := LicenseCheck(&req)
+			res := License(&req)
 
 			if !scut.ValidateTestReturn(t, tt.name, &tt.expected, &res, &dl) {
 				t.Fail()
