92a117e2792f2a58c6f58e2db44e4e0fe3da5194
aidenwang <yidingwang9867@gmail.com>
2022-07-11 11:31:06 -0700

    temp

--- pkg/check-depdiff/dependencies.go
+++ pkg/check-depdiff/dependencies.go
@@ -14,12 +14,17 @@
 
 package depdiff
 
+import "github.com/ossf/scorecard/v4/pkg"
+
 // ChangeType is the change type (added, updated, removed) of a dependency.
 type ChangeType string
 
 const (
-	Added   ChangeType = "added"
+	// Added suggests the dependency is a new one.
+	Added ChangeType = "added"
+	// Updated suggests the dependency is bumped from an old version.
 	Updated ChangeType = "updated"
+	// Removed suggests the dependency is removed.
 	Removed ChangeType = "removed"
 )
 
@@ -33,26 +38,54 @@ func (ct *ChangeType) IsValid() bool {
 	}
 }
 
-// Dependency is a dependency.
-type Dependency struct {
-	// ChangeType indicates whether the dependency is added, updated, or removed.
-	ChangeType ChangeType `json:"change_type"`
-
-	// ManifestFileName is the name of the manifest file of the dependency, such as go.mod for Go.
-	ManifestFileName string `json:"manifest"`
-
-	// Ecosystem is the name of the package management system, such as NPM, GO, PYPI.
-	Ecosystem string `json:"ecosystem"`
-
-	// Name is the name of the dependency.
-	Name string `json:"name"`
-
-	// Version is the package version of the dependency.
-	Version string `json:"version"`
-
+// rawDependency is the Dependency structure that is used to receive
+// the raw results from the GitHub Dependency Review API.
+type rawDependency struct {
 	// Package URL is a short link for a package.
 	PackageURL *string `json:"package_url"`
 
 	// SrcRepoURL is the source repository URL of the dependency.
 	SrcRepoURL *string `json:"source_repository_url"`
+
+	// ChangeType indicates whether the dependency is added, updated, or removed.
+	ChangeType *ChangeType `json:"change_type"`
+
+	// ManifestFileName is the name of the manifest file of the dependency, such as go.mod for Go.
+	ManifestPath *string `json:"manifest"`
+
+	// Ecosystem is the name of the package management system, such as NPM, GO, PYPI.
+	Ecosystem *string `json:"ecosystem"`
+
+	// Name is the name of the dependency.
+	Name string `json:"name"`
+
+	// Version is the package version of the dependency.
+	Version *string `json:"version"`
+}
+
+// Dependency is the dependency structure used in the returned results.
+type Dependency struct {
+	// Package URL is a short link for a package.
+	PackageURL *string `json:"packageUrl"`
+
+	// SrcRepoURL is the source repository URL of the dependency.
+	SrcRepoURL *string `json:"srcRepoUrl"`
+
+	// ChangeType indicates whether the dependency is added, updated, or removed.
+	ChangeType *ChangeType `json:"changeType"`
+
+	// ManifestFileName is the name of the manifest file of the dependency, such as go.mod for Go.
+	ManifestPath *string `json:"manifest"`
+
+	// Ecosystem is the name of the package management system, such as NPM, GO, PYPI.
+	Ecosystem *string `json:"ecosystem"`
+
+	// Name is the name of the dependency.
+	Name string `json:"name"`
+
+	// Version is the package version of the dependency.
+	Version *string `json:"version"`
+
+	// ScReresults is the scorecard result for the dependency repo.
+	ScReresults pkg.ScorecardResult `json:"scorecardResults"`
 }
