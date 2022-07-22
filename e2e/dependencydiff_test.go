4b9e13892ee0b9ec410c2dd05b4fb23496f401c0
aidenwang <yidingwang9867@gmail.com>
2022-07-22 13:58:25 -0700

    save

--- e2e/dependencydiff_test.go
+++ e2e/dependencydiff_test.go
@@ -44,7 +44,7 @@ var _ = Describe("E2E TEST:"+dependencydiff.Depdiff, func() {
 			results, err := dependencydiff.GetDependencyDiffResults(
 				ctx,
 				repoURI,
-				base, base,
+				base, head,
 				checksToRun,
 				changeTypesToCheck,
 			)
@@ -62,7 +62,7 @@ var _ = Describe("E2E TEST:"+dependencydiff.Depdiff, func() {
 			results, err := dependencydiff.GetDependencyDiffResults(
 				ctx,
 				repoURI,
-				base, head,
+				base, base,
 				checksToRun,
 				changeTypesToCheck,
 			)
