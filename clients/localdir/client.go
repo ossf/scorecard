e23ee84db0398468a17c41ee9755e0f8ab903e79
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2022-07-22 21:37:17 -0500

    ✨  Export Scorecards results for API (#2081)
    * :seedling: Export Scorecards results for API
    
    - Exporting the Scorecard results for the scorecard API.
    - The code exports as result.json without the commit SHA and also with
      the commit SHA.
    
    * Some cleanup and tweaks.
    
    * Some cleanup and tweaks.

--- clients/localdir/client.go
+++ clients/localdir/client.go
@@ -219,7 +219,7 @@ func (client *localDirClient) Close() error {
 }
 
 // ListProgrammingLanguages implements RepoClient.ListProgrammingLanguages.
-// TODO: add ListProgrammingLanguages support for local directories
+// TODO: add ListProgrammingLanguages support for local directories.
 func (client *localDirClient) ListProgrammingLanguages() ([]clients.Language, error) {
 	return nil, fmt.Errorf("ListProgrammingLanguages: %w", clients.ErrUnsupportedFeature)
 }
