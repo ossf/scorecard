f6b1d96717ba8a1588b8f6ebe8af7a959db6e83b
aidenwang <yidingwang9867@gmail.com>
2022-07-27 13:33:04 -0700

    save

--- checks/raw/fuzzing.go
+++ checks/raw/fuzzing.go
@@ -108,7 +108,6 @@ func Fuzzing(c *checker.CheckRequest) (checker.FuzzingData, error) {
 		return checker.FuzzingData{}, fmt.Errorf("cannot get langs of repo: %w", err)
 	}
 	prominentLangs := getProminentLanguages(langs)
-
 	for _, lang := range prominentLangs {
 		usingFuzzFunc, files, e := checkFuzzFunc(c, lang)
 		if e != nil {
@@ -234,18 +233,20 @@ func getProminentLanguages(langs []clients.Language) []clients.LanguageName {
 		return nil
 	}
 	totalLoC := 0
+	// Use a map to record languages and their lines of code to drop potential duplicates.
+	langMap := map[clients.LanguageName]int{}
 	for _, l := range langs {
 		totalLoC += l.NumLines
+		langMap[l.Name] += l.NumLines
 	}
-	// Var avgLoC calculates the average lines of code in the current repo,
-	// and it can stay as an int, no need for a float value.
+	// Calculate the average lines of code in the current repo.
+	// This var can stay as an int, no need for a precise float value.
 	avgLoC := totalLoC / numLangs
-
 	// Languages that have lines of code above average will be considered prominent.
 	ret := []clients.LanguageName{}
-	for _, l := range langs {
-		if l.NumLines >= avgLoC {
-			lang := clients.LanguageName(strings.ToLower(string(l.Name)))
+	for lName, loC := range langMap {
+		if loC >= avgLoC {
+			lang := clients.LanguageName(strings.ToLower(string(lName)))
 			ret = append(ret, lang)
 		}
 	}
