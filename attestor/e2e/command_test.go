94fee66e505d01b73b28ebfe35da41e22be2ab5a
raghavkaul <8695110+raghavkaul@users.noreply.github.com>
2022-11-01 14:30:17 -0400

    ✨ CLI for scorecard-attestor (#2309)
    * Reorganize
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Working commit
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Compile with local scorecard; go mod tidy
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Add signing code
    
    Heavily borrowed from https://github.com/grafeas/kritis/blob/master/cmd/kritis/signer/main.go
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Update deps
    
    * Naming
    * Makefile
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Edit license, add lint.yml
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * checks: go mod tidy, license
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Address PR comments
    
    * Split into checker/signer files
    * Naming convention
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * License, remove golangci.yml
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Address PR comments
    
    * Use cobra
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Add tests for root command
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Filter out checks that aren't needed for policy evaluation
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Add `make` targets for attestor; submit coverage stats
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Improvements
    
    * Use sclog instead of glog
    * Remove unneeded subcommands
    * Formatting
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Flags: Make note-name constant and fix messaging
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Remove SupportedRequestTypes
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * go mod tidy
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * go mod tidy, makefile
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    * Fix GH actions run
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    
    Signed-off-by: Raghav Kaul <raghavkaul@google.com>
    Signed-off-by: latortuga71 <christopheralonso1@gmail.com>

---
+++ attestor/e2e/command_test.go
@@ -0,0 +1,59 @@
+// Copyright 2022 Security Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package e2e
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/ossf/scorecard-attestor/command"
+	"github.com/spf13/cobra"
+)
+
+func execute(t *testing.T, c *cobra.Command, args ...string) (string, error) {
+	t.Helper()
+
+	buf := new(bytes.Buffer)
+	c.SetOut(buf)
+	c.SetErr(buf)
+	c.SetArgs(args)
+
+	err := c.Execute()
+	return strings.TrimSpace(buf.String()), err
+}
+
+func TestRootCmd(t *testing.T) {
+	tt := []struct {
+		name string
+		args []string
+	}{
+		{
+			name: "test check-only from root",
+			args: []string{
+				"verify",
+				"--policy=../policy/testdata/policy-binauthz.yaml",
+				"--repo-url=https://github.com/ossf-tests/scorecard",
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		_, err := execute(t, command.RootCmd, tc.args...)
+
+		if err != nil {
+			t.Fatalf("%s: %s", tc.name, err)
+		}
+	}
+}
