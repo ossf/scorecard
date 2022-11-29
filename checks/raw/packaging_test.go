d38e9d00fc9a986e9a806c5070bf2f1a775d773a
laurentsimon <laurentsimon@google.com>
2022-11-29 23:48:22 +0000

    update

---
+++ checks/raw/packaging_test.go
@@ -0,0 +1,93 @@
+// Copyright 2021 Security Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package raw
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/ossf/scorecard/v4/checker"
+	mockrepo "github.com/ossf/scorecard/v4/clients/mockclients"
+	scut "github.com/ossf/scorecard/v4/utests"
+)
+
+func Test_Packaging(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		filenames []string
+		err       error
+		score     int
+	}{
+		{
+			name:      "Go project root dir",
+			filenames: []string{"go.mod"},
+			score:     10,
+		},
+		{
+			name:      "Go project not root dir",
+			filenames: []string{"path/go.mod"},
+			score:     0,
+		},
+		// TODO(): add unit tests for packaging
+	}
+	for _, tt := range tests {
+		tt := tt // Re-initializing variable so it is not changed while executing the closure below
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			mockRepoClient := mockrepo.NewMockRepoClient(ctrl)
+			mockRepoClient.EXPECT().ListFiles(gomock.Any()).Return(tt.filenames, nil).AnyTimes()
+			mockRepoClient.EXPECT().GetFileContent(gomock.Any()).Return(nil, nil).AnyTimes()
+
+			mockRepo := mockrepo.NewMockRepo(ctrl)
+			mockRepo.EXPECT().URI().Return("github.com/repo/name").AnyTimes()
+
+			dl := scut.TestDetailLogger{}
+			req := checker.CheckRequest{
+				RepoClient: mockRepoClient,
+				Ctx:        context.TODO(),
+				Repo:       mockRepo,
+				Dlogger:    &dl,
+			}
+
+			pkgs, err := Packaging(&req)
+
+			if !errCmp(err, tt.err) {
+				t.Errorf(cmp.Diff(err, tt.err, cmpopts.EquateErrors()))
+			}
+			if tt.err != nil {
+				return
+			}
+
+			expectedLength := 1
+			fmt.Println(tt.name, tt.score, expectedLength, len(pkgs.Packages))
+			if len(pkgs.Packages) != expectedLength {
+				t.Errorf(cmp.Diff(len(pkgs.Packages), expectedLength))
+			}
+
+			if pkgs.Packages[0].Name == nil && tt.score != 0 {
+				t.Errorf("no name found")
+			}
+		})
+	}
+}
