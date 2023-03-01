843e6d37ff1dc7fedfb51c6d8b4942b24b773e87
Spencer Schrock <sschrock@google.com>
2023-03-01 13:34:14 -0800

    Change retry-after logic to support any status code. Disable troublesome checks.
    Signed-off-by: Spencer Schrock <sschrock@google.com>

--- clients/githubrepo/roundtripper/rate_limit.go
+++ clients/githubrepo/roundtripper/rate_limit.go
@@ -15,9 +15,7 @@
 package roundtripper
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
 	"time"
@@ -47,25 +45,11 @@ func (gh *rateLimitTransport) RoundTrip(r *http.Request) (*http.Response, error)
 		return nil, sce.WithMessage(sce.ErrScorecardInternal, fmt.Sprintf("innerTransport.RoundTrip: %v", err))
 	}
 
-	if resp.StatusCode == http.StatusForbidden {
-		data, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		resp.Body = io.NopCloser(bytes.NewBuffer(data))
-		if err != nil || data == nil {
-			return resp, nil
-		}
-		if bytes.Contains(data, []byte("You have exceeded a secondary rate limit")) {
-			retryValue := resp.Header.Get("Retry-After")
-			retryAfter, err := strconv.Atoi(retryValue)
-			if err != nil {
-				retryAfter = 60
-			}
-			gh.logger.Info(fmt.Sprintf("Secondary rate limit exceeded. Waiting %d to retry...", retryAfter))
-			time.Sleep(time.Duration(retryAfter) * time.Second)
-			gh.logger.Info("Secondary rate limit exceeded. Retrying...")
-			return gh.RoundTrip(r)
-		}
-		gh.logger.Info("Non secondary rate limit 403")
+	retryValue := resp.Header.Get("Retry-After")
+	if retryAfter, err := strconv.Atoi(retryValue); err == nil { // if NO error
+		gh.logger.Info(fmt.Sprintf("Retry-After header set. Waiting %d to retry...", retryAfter))
+		time.Sleep(time.Duration(retryAfter) * time.Second)
+		gh.logger.Info("Retry-After header set. Retrying...")
 	}
 
 	rateLimit := resp.Header.Get("X-RateLimit-Remaining")
