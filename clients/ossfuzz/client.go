60e464bbc5cca312ab4ed17b9d20e404b0bcb383
Spencer Schrock <sschrock@google.com>
2023-03-02 21:21:31 -0800

    Split lazy and eager behavior into different implementations.
    Signed-off-by: Spencer Schrock <sschrock@google.com>

--- clients/ossfuzz/client.go
+++ clients/ossfuzz/client.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -29,18 +30,33 @@ import (
 	"github.com/ossf/scorecard/v4/clients"
 )
 
-type ossFuzzClient struct {
+var (
+	errUnreachableStatusFile = errors.New("could not fetch OSS Fuzz status file")
+	errMalformedURL          = errors.New("malformed repo url")
+)
+
+type commonClient struct {
 	statusURL string
-	err       error
-	contents  []byte
-	projects  map[string]bool
-	once      sync.Once
+}
+
+type lazyClient struct {
+	commonClient
+	err      error
+	contents []byte
+	once     sync.Once
+}
+
+type eagerClient struct {
+	projects map[string]bool
+	commonClient
 }
 
 // CreateOSSFuzzClient returns a client which implements RepoClient interface.
 func CreateOSSFuzzClient(ctx context.Context, ossFuzzStatusURL string) clients.RepoClient {
-	return &ossFuzzClient{
-		statusURL: ossFuzzStatusURL,
+	return &lazyClient{
+		commonClient: commonClient{
+			statusURL: ossFuzzStatusURL,
+		},
 	}
 }
 
@@ -56,20 +72,20 @@ func CreateEagerOSSFuzzClient(ctx context.Context, ossFuzzStatusURL string) (cli
 		return nil, err
 	}
 	status := ossFuzzStatus{}
-	err = json.Unmarshal(contents, &status)
-	if err != nil {
-		return nil, fmt.Errorf("parse OSS Fuzz status file: %w", err)
+	if err = json.Unmarshal(contents, &status); err != nil {
+		return nil, fmt.Errorf("parse status file: %w", err)
 	}
 
-	c := ossFuzzClient{
-		statusURL: ossFuzzStatusURL,
-		projects:  map[string]bool{},
+	c := eagerClient{
+		commonClient: commonClient{
+			statusURL: ossFuzzStatusURL,
+		},
+		projects: map[string]bool{},
 	}
-	// consume the sync.Once for the later search
-	c.once.Do(func() {})
+
 	for i := range status.Projects {
 		repoURI := status.Projects[i].RepoURI
-		normalizedRepoURI, err := parseRepo(repoURI)
+		normalizedRepoURI, err := normalize(repoURI)
 		if err != nil {
 			continue
 		}
@@ -79,7 +95,7 @@ func CreateEagerOSSFuzzClient(ctx context.Context, ossFuzzStatusURL string) (cli
 }
 
 // Search implements RepoClient.Search.
-func (c *ossFuzzClient) Search(request clients.SearchRequest) (clients.SearchResponse, error) {
+func (c *lazyClient) Search(request clients.SearchRequest) (clients.SearchResponse, error) {
 	c.once.Do(func() {
 		c.contents, c.err = fetchStatusFile(c.statusURL)
 	})
@@ -87,41 +103,48 @@ func (c *ossFuzzClient) Search(request clients.SearchRequest) (clients.SearchRes
 		return clients.SearchResponse{}, c.err
 	}
 	var sr clients.SearchResponse
-	// the eager client may have pre-calculated these results
-	if c.projects != nil {
-		if c.projects[request.Query] {
-			sr.Hits = 1
-		}
-		return sr, nil
-	}
-	projectURI := request.Query
-	if bytes.Contains(c.contents, []byte(projectURI)) {
+	projectURI := []byte(request.Query)
+	if bytes.Contains(c.contents, projectURI) {
 		sr.Hits = 1
 	}
 	return sr, nil
 }
 
-func fetchStatusFile(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+// Search implements RepoClient.Search.
+func (c *eagerClient) Search(request clients.SearchRequest) (clients.SearchResponse, error) {
+	var sr clients.SearchResponse
+	if c.projects[request.Query] {
+		sr.Hits = 1
+	}
+	return sr, nil
+}
+
+func fetchStatusFile(uri string) ([]byte, error) {
+	//nolint:gosec // URI comes from a constant or a test HTTP server, not user input
+	resp, err := http.Get(uri)
 	if err != nil {
 		return nil, fmt.Errorf("http.Get: %w", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode >= 400 {
-		return nil, fmt.Errorf("fetch OSS-Fuzz project list: %s", resp.Status)
+		return nil, fmt.Errorf("%s: %w", resp.Status, errUnreachableStatusFile)
 	}
-	return io.ReadAll(resp.Body)
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("io.ReadAll: %w", err)
+	}
+	return b, nil
 }
 
-func parseRepo(s string) (string, error) {
-	u, e := url.Parse(s)
-	if e != nil {
-		return "", fmt.Errorf("url.Parse: %w", e)
+func normalize(rawURL string) (string, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("url.Parse: %w", err)
 	}
 	const splitLen = 2
 	split := strings.SplitN(strings.Trim(u.Path, "/"), "/", splitLen)
 	if len(split) != splitLen {
-		return "", fmt.Errorf("malformed url: %s", s)
+		return "", fmt.Errorf("%s: %w", rawURL, errMalformedURL)
 	}
 	org := split[0]
 	repo := strings.TrimSuffix(split[1], ".git")
@@ -129,111 +152,111 @@ func parseRepo(s string) (string, error) {
 }
 
 // URI implements RepoClient.URI.
-func (c *ossFuzzClient) URI() string {
+func (c *commonClient) URI() string {
 	return c.statusURL
 }
 
 // InitRepo sets up the local repo.
-func (c *ossFuzzClient) InitRepo(inputRepo clients.Repo, commitSHA string, commitDepth int) error {
+func (c *commonClient) InitRepo(inputRepo clients.Repo, commitSHA string, commitDepth int) error {
 	return fmt.Errorf("InitRepo: %w", clients.ErrUnsupportedFeature)
 }
 
 // IsArchived implements RepoClient.IsArchived.
-func (c *ossFuzzClient) IsArchived() (bool, error) {
+func (c *commonClient) IsArchived() (bool, error) {
 	return false, fmt.Errorf("IsArchived: %w", clients.ErrUnsupportedFeature)
 }
 
 // LocalPath implements RepoClient.LocalPath.
-func (c *ossFuzzClient) LocalPath() (string, error) {
+func (c *commonClient) LocalPath() (string, error) {
 	return "", fmt.Errorf("LocalPath: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListFiles implements RepoClient.ListFiles.
-func (c *ossFuzzClient) ListFiles(predicate func(string) (bool, error)) ([]string, error) {
+func (c *commonClient) ListFiles(predicate func(string) (bool, error)) ([]string, error) {
 	return nil, fmt.Errorf("ListFiles: %w", clients.ErrUnsupportedFeature)
 }
 
 // GetFileContent implements RepoClient.GetFileContent.
-func (c *ossFuzzClient) GetFileContent(filename string) ([]byte, error) {
+func (c *commonClient) GetFileContent(filename string) ([]byte, error) {
 	return nil, fmt.Errorf("GetFileContent: %w", clients.ErrUnsupportedFeature)
 }
 
 // GetBranch implements RepoClient.GetBranch.
-func (c *ossFuzzClient) GetBranch(branch string) (*clients.BranchRef, error) {
+func (c *commonClient) GetBranch(branch string) (*clients.BranchRef, error) {
 	return nil, fmt.Errorf("GetBranch: %w", clients.ErrUnsupportedFeature)
 }
 
 // GetDefaultBranch implements RepoClient.GetDefaultBranch.
-func (c *ossFuzzClient) GetDefaultBranch() (*clients.BranchRef, error) {
+func (c *commonClient) GetDefaultBranch() (*clients.BranchRef, error) {
 	return nil, fmt.Errorf("GetDefaultBranch: %w", clients.ErrUnsupportedFeature)
 }
 
 // GetDefaultBranchName implements RepoClient.GetDefaultBranchName.
-func (c *ossFuzzClient) GetDefaultBranchName() (string, error) {
+func (c *commonClient) GetDefaultBranchName() (string, error) {
 	return "", fmt.Errorf("GetDefaultBranchName: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListCommits implements RepoClient.ListCommits.
-func (c *ossFuzzClient) ListCommits() ([]clients.Commit, error) {
+func (c *commonClient) ListCommits() ([]clients.Commit, error) {
 	return nil, fmt.Errorf("ListCommits: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListIssues implements RepoClient.ListIssues.
-func (c *ossFuzzClient) ListIssues() ([]clients.Issue, error) {
+func (c *commonClient) ListIssues() ([]clients.Issue, error) {
 	return nil, fmt.Errorf("ListIssues: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListReleases implements RepoClient.ListReleases.
-func (c *ossFuzzClient) ListReleases() ([]clients.Release, error) {
+func (c *commonClient) ListReleases() ([]clients.Release, error) {
 	return nil, fmt.Errorf("ListReleases: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListContributors implements RepoClient.ListContributors.
-func (c *ossFuzzClient) ListContributors() ([]clients.User, error) {
+func (c *commonClient) ListContributors() ([]clients.User, error) {
 	return nil, fmt.Errorf("ListContributors: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListSuccessfulWorkflowRuns implements RepoClient.WorkflowRunsByFilename.
-func (c *ossFuzzClient) ListSuccessfulWorkflowRuns(filename string) ([]clients.WorkflowRun, error) {
+func (c *commonClient) ListSuccessfulWorkflowRuns(filename string) ([]clients.WorkflowRun, error) {
 	return nil, fmt.Errorf("ListSuccessfulWorkflowRuns: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListCheckRunsForRef implements RepoClient.ListCheckRunsForRef.
-func (c *ossFuzzClient) ListCheckRunsForRef(ref string) ([]clients.CheckRun, error) {
+func (c *commonClient) ListCheckRunsForRef(ref string) ([]clients.CheckRun, error) {
 	return nil, fmt.Errorf("ListCheckRunsForRef: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListStatuses implements RepoClient.ListStatuses.
-func (c *ossFuzzClient) ListStatuses(ref string) ([]clients.Status, error) {
+func (c *commonClient) ListStatuses(ref string) ([]clients.Status, error) {
 	return nil, fmt.Errorf("ListStatuses: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListWebhooks implements RepoClient.ListWebhooks.
-func (c *ossFuzzClient) ListWebhooks() ([]clients.Webhook, error) {
+func (c *commonClient) ListWebhooks() ([]clients.Webhook, error) {
 	return nil, fmt.Errorf("ListWebhooks: %w", clients.ErrUnsupportedFeature)
 }
 
 // SearchCommits implements RepoClient.SearchCommits.
-func (c *ossFuzzClient) SearchCommits(request clients.SearchCommitsOptions) ([]clients.Commit, error) {
+func (c *commonClient) SearchCommits(request clients.SearchCommitsOptions) ([]clients.Commit, error) {
 	return nil, fmt.Errorf("SearchCommits: %w", clients.ErrUnsupportedFeature)
 }
 
 // Close implements RepoClient.Close.
-func (c *ossFuzzClient) Close() error {
+func (c *commonClient) Close() error {
 	return nil
 }
 
 // ListProgrammingLanguages implements RepoClient.ListProgrammingLanguages.
-func (c *ossFuzzClient) ListProgrammingLanguages() ([]clients.Language, error) {
+func (c *commonClient) ListProgrammingLanguages() ([]clients.Language, error) {
 	return nil, fmt.Errorf("ListProgrammingLanguages: %w", clients.ErrUnsupportedFeature)
 }
 
 // ListLicenses implements RepoClient.ListLicenses.
-func (c *ossFuzzClient) ListLicenses() ([]clients.License, error) {
+func (c *commonClient) ListLicenses() ([]clients.License, error) {
 	return nil, fmt.Errorf("ListLicenses: %w", clients.ErrUnsupportedFeature)
 }
 
 // GetCreatedAt implements RepoClient.GetCreatedAt.
-func (c *ossFuzzClient) GetCreatedAt() (time.Time, error) {
+func (c *commonClient) GetCreatedAt() (time.Time, error) {
 	return time.Time{}, fmt.Errorf("GetCreatedAt: %w", clients.ErrUnsupportedFeature)
 }
