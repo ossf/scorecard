23617ab7d8b7119cd7df9d87b9d36b29e45b2c48
Gabriela Gutierrez <gabigutierrez@google.com>
2023-03-10 22:03:40 +0000

    test: Multi editable pip install in GHA
    Signed-off-by: Gabriela Gutierrez <gabigutierrez@google.com>

--- checks/raw/pinned_dependencies_test.go
+++ checks/raw/pinned_dependencies_test.go
@@ -261,7 +261,7 @@ func TestGithubWorkflowPkgManagerPinning(t *testing.T) {
 		{
 			name:     "npm packages without verification",
 			filename: "./testdata/.github/workflows/github-workflow-pkg-managers.yaml",
-			warns:    45,
+			warns:    46,
 		},
 	}
 	for _, tt := range tests {
