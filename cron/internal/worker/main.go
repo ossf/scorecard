0a3467c6b1acdd18744d703076d133c2820b490f
Spencer Schrock <sschrock@google.com>
2023-04-11 14:52:56 -0700

    🌱 Add instructions to test cron controller + worker locally (#2817)
    * Add GitLab test repos.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Add test GitLab projects to release controller.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * worker gitlab WIP
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Read config in worker.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Use UTC time for shards.
    
    This avoids issues when the controller and worker timezones differ.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * update directions for gcs fake
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * update readme
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Undo gitlab parts, which will be its own PR.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Clarify project and config files are placeholders.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * remove accidentally added whitespace
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * clarify code change with comment.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    * Minor edits.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    
    ---------
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>
    Signed-off-by: Avishay <avishay.balter@gmail.com>

--- cron/internal/worker/main.go
+++ cron/internal/worker/main.go
@@ -266,6 +266,9 @@ func startMetricsExporter() (monitoring.Exporter, error) {
 
 func main() {
 	flag.Parse()
+	if err := config.ReadConfig(); err != nil {
+		panic(err)
+	}
 	sw, err := newScorecardWorker()
 	if err != nil {
 		panic(err)
