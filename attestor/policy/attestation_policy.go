00cc90c1e5c08a9e2578e5bf104ed4e1b965d5e7
Gabriela Gutierrez <gabigutierrez@google.com>
2023-04-18 16:36:43 -0300

    fix: Change attestion policy check for unpinned dependencies
    Signed-off-by: Gabriela Gutierrez <gabigutierrez@google.com>

--- attestor/policy/attestation_policy.go
+++ attestor/policy/attestation_policy.go
@@ -267,7 +267,7 @@ func CheckNoUnpinnedDependencies(
 ) (PolicyResult, error) {
 	for i := range results.PinningDependenciesResults.Dependencies {
 		dep := results.PinningDependenciesResults.Dependencies[i]
-		if (dep.PinnedAt == nil || *dep.PinnedAt == "") && !isUnpinnedDependencyAllowed(dep, allowed) {
+		if *dep.Pinned == false && !isUnpinnedDependencyAllowed(dep, allowed) {
 			logger.Info(fmt.Sprintf("found unpinned dependency %v", dep))
 			return Fail, nil
 		}
