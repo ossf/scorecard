2953b409865aa4dfdd2520cac8760631bc942471
laurentsimon <laurentsimon@google.com>
2023-05-04 17:45:35 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

---
+++ probes/toolSonatypeLiftInstalled/impl.go
@@ -0,0 +1,44 @@
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package toolSonatypeLiftInstalled
+
+import (
+	"embed"
+
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+	"github.com/ossf/scorecard/v4/probes/utils"
+)
+
+//go:embed *.yml
+var fs embed.FS
+
+const probe = "toolSonatypeLiftInstalled"
+
+type sonatypeLyft struct{}
+
+func (t sonatypeLyft) Name() string {
+	return "Sonatype Lift"
+}
+
+func (t sonatypeLyft) Matches(tool checker.Tool) bool {
+	return t.Name() == tool.Name
+}
+
+func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
+	tools := raw.DependencyUpdateToolResults.Tools
+	var matcher sonatypeLyft
+	// Check whether Sona Lyft tool is installed on the repo,
+	// and create the correponding findings.
+	return utils.ToolsRun(tools, fs, probe,
+		finding.OutcomePositive, finding.OutcomeNegative, matcher)
+}
