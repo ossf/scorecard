67dda9e642fbae0687c502598f6f9679f6c0a635
laurentsimon <laurentsimon@google.com>
2023-05-22 23:41:45 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- probes/toolSonatypeLiftInstalled/impl.go
+++ probes/toolSonatypeLiftInstalled/impl.go
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-// Copyright 2022 OpenSSF Scorecard Authors
+// Copyright 2023 OpenSSF Scorecard Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-=======
->>>>>>> 2953b409 (update)
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-<<<<<<< HEAD
 // nolint:stylecheck
-=======
->>>>>>> 2953b409 (update)
 package toolSonatypeLiftInstalled
 
 import (
