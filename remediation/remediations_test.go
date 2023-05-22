f88a1f18735562d38efb20600dd1049ecf5d0dc1
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2023-05-22 11:41:50 -0500

    Fixed the codereview comment.
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- remediation/remediations_test.go
+++ remediation/remediations_test.go
@@ -178,7 +178,9 @@ func TestCreateWorkflowPinningRemediation(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			r := RemediationMetadata{
 				Branch: tt.branch,
 				Repo:   tt.repo,
