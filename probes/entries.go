8c68bd5e6c8926273a87485f1ae81b5f96257857
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2023-05-22 18:13:24 -0700

    ✨  [experimental] Add probe code and support for Tool-Update-Dependency (#2944)
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    ---------
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>

---
+++ probes/entries.go
@@ -0,0 +1,59 @@
+// Copyright 2023 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package probes
+
+import (
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+	"github.com/ossf/scorecard/v4/probes/toolDependabotInstalled"
+	"github.com/ossf/scorecard/v4/probes/toolPyUpInstalled"
+	"github.com/ossf/scorecard/v4/probes/toolRenovateInstalled"
+	"github.com/ossf/scorecard/v4/probes/toolSonatypeLiftInstalled"
+)
+
+// ProbeImpl is the implementation of a probe.
+type ProbeImpl func(*checker.RawResults) ([]finding.Finding, string, error)
+
+var (
+	// All represents all the probes.
+	All []ProbeImpl
+	// DependencyToolUpdates is all the probes for the
+	// DpendencyUpdateTool check.
+	DependencyToolUpdates = []ProbeImpl{
+		toolRenovateInstalled.Run,
+		toolDependabotInstalled.Run,
+		toolPyUpInstalled.Run,
+		toolSonatypeLiftInstalled.Run,
+	}
+)
+
+//nolint:gochecknoinits
+func init() {
+	All = concatMultipleProbes([][]ProbeImpl{
+		DependencyToolUpdates,
+	})
+}
+
+func concatMultipleProbes(slices [][]ProbeImpl) []ProbeImpl {
+	var totalLen int
+	for _, s := range slices {
+		totalLen += len(s)
+	}
+	tmp := make([]ProbeImpl, 0, totalLen)
+	for _, s := range slices {
+		tmp = append(tmp, s...)
+	}
+	return tmp
+}
