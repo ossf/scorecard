1faa27732b7f3fc0c3c418648bffc1256eb2c213
André Backman <andrebackmann@gmail.com>
2023-07-04 16:21:54 +0300

    print raw results
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/minTwoCodeReviewers/impl.go
+++ probes/minTwoCodeReviewers/impl.go
@@ -2,3 +2,24 @@
 // Licensed under the Apache License 2.0
 // SPDX-License-Identifier: Apache-2.0
 
+package minTwoCodeReviewers
+
+import (
+	"embed"
+
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+	"github.com/ossf/scorecard/v4/probes/utils"
+
+	"fmt" // extra abackman rm remove
+)
+
+var fs embed.FS
+
+const probe = "minTwoCodeReviewers"
+
+func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
+	reviewData := raw.CodeReviewResults.DefaultBranchChangesets
+	fmt.Printf("%v\n\n", reviewData)
+	return utils.CodeReviewRun(reviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
+}
