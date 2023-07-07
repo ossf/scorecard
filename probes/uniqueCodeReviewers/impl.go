2f5838e09406f36dff80247b3d7281b43ccb0613
André Backman <andrebackmann@gmail.com>
2023-07-07 13:54:04 +0300

    rename probe CodeReviewers
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/uniqueCodeReviewers/impl.go
+++ probes/uniqueCodeReviewers/impl.go
@@ -2,7 +2,7 @@
 // Licensed under the Apache License 2.0
 // SPDX-License-Identifier: Apache-2.0
 
-package minTwoCodeReviewers
+package minimumCodeReviewers
 
 import (
 	"embed"
@@ -11,15 +11,15 @@ import (
 	"github.com/ossf/scorecard/v4/finding"
 	"github.com/ossf/scorecard/v4/probes/utils"
 
-	"fmt" // extra abackman rm remove
+	//"fmt" // extra abackman rm remove
 )
 
 var fs embed.FS
 
-const probe = "minTwoCodeReviewers"
+const probe = "minimumCodeReviewers"
 
 func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
 	reviewData := raw.CodeReviewResults.DefaultBranchChangesets
-	fmt.Printf("%v\n\n", reviewData)
+	//fmt.Printf("%v\n\n", reviewData)
 	return utils.CodeReviewRun(reviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
 }
