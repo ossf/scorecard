e1f42248a4d0bfc1959af6dc08e0cc1a89405dfe
André Backman <andrebackmann@gmail.com>
2023-07-10 13:47:41 +0300

    printing all findings, work out where to concatenate them
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/utils/code_review.go
+++ probes/utils/code_review.go
@@ -7,19 +7,60 @@ package utils
 import (
 	"embed"
 	"fmt"
-
+	//"os"
 	"github.com/ossf/scorecard/v4/checker"
 	"github.com/ossf/scorecard/v4/finding"
 )
 
+var minimumReviewers = 2
+
+// Looks through the data and validates author and reviewers of a changeset
 func CodeReviewRun(reviewData []checker.Changeset, fs embed.FS, probeID string,
-	foundOutcome, notFoundOutcome finding.Outcome,
-) ([]finding.Finding, string, error) {
-	var findings []finding.Finding
-	for i := range reviewData {
-		data := &reviewData[i]
-		fmt.Printf("%dABACKMAN%v\n\n", i, data)
-		//fmt.Printf("ABACKMAN%v", findings)
+	positiveOutcome, negativeOutcome finding.Outcome,
+	) ([]finding.Finding, string, error) {
+		var findings []finding.Finding
+		for i := range reviewData {
+			data := &reviewData[i]
+			if data.ReviewPlatform == "Unknown" && data.Author.Login == "" {
+				continue
+		}
+		reviewersList := make([]string, len(data.Reviews))
+		for i := range data.Reviews {
+			reviewersList[i] = data.Reviews[i].Author.Login
+		}
+		numReviewers := uniqueReviewers(data.Author.Login, reviewersList)
+		if numReviewers >= minimumReviewers {
+			f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found", numReviewers),
+			nil, positiveOutcome)
+			if err != nil {
+				return nil, probeID, fmt.Errorf("create finding: %w", err)
+			}
+			findings = append(findings, *f)
+			} else {
+				f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found", numReviewers),
+				nil, negativeOutcome)
+				if err != nil {
+					return nil, probeID, fmt.Errorf("create finding: %w", err)
+				}
+				findings = append(findings, *f)
+			}
+		}
+		return findings, probeID, nil
 	}
-	return findings, probeID, nil
-}
+	
+	// returns the number of unique reviewers that aren't the changeset author
+	func uniqueReviewers(authorLogin string, reviewers []string) int {
+		uniqueReviewers := 0
+		for i := range reviewers {
+			duplicateCount := 0
+			for j := range reviewers {
+				if reviewers[j] == reviewers[i] && j > i {
+					duplicateCount++
+				}
+			}
+			if reviewers[i] != authorLogin && duplicateCount == 0 {
+				uniqueReviewers++
+			}
+		}
+		return uniqueReviewers
+	}
