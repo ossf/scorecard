de2ae3d5d9f42eb22ccc487206d268f9cf975fae
André Backman <andrebackmann@gmail.com>
2023-07-10 16:19:36 +0300

    concatenated findings to one single finding, outcome is based on the least found unique reviewers
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/utils/code_review.go
+++ probes/utils/code_review.go
@@ -18,49 +18,53 @@ var minimumReviewers = 2
 func CodeReviewRun(reviewData []checker.Changeset, fs embed.FS, probeID string,
 	positiveOutcome, negativeOutcome finding.Outcome,
 	) ([]finding.Finding, string, error) {
-		var findings []finding.Finding
-		for i := range reviewData {
-			data := &reviewData[i]
-			if data.ReviewPlatform == "Unknown" && data.Author.Login == "" {
-				continue
+	var findings []finding.Finding
+	leastFoundReviewers := 0
+	for i := range reviewData {
+		data := &reviewData[i]
+		if data.ReviewPlatform == "Unknown" && data.Author.Login == "" {
+			continue
 		}
 		reviewersList := make([]string, len(data.Reviews))
 		for i := range data.Reviews {
 			reviewersList[i] = data.Reviews[i].Author.Login
 		}
 		numReviewers := uniqueReviewers(data.Author.Login, reviewersList)
-		if numReviewers >= minimumReviewers {
-			f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found", numReviewers),
-			nil, positiveOutcome)
-			if err != nil {
-				return nil, probeID, fmt.Errorf("create finding: %w", err)
-			}
-			findings = append(findings, *f)
-			} else {
-				f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found", numReviewers),
-				nil, negativeOutcome)
-				if err != nil {
-					return nil, probeID, fmt.Errorf("create finding: %w", err)
-				}
-				findings = append(findings, *f)
-			}
+		if i == 0 || numReviewers < leastFoundReviewers {
+			leastFoundReviewers = numReviewers
 		}
-		return findings, probeID, nil
 	}
+	if leastFoundReviewers >= minimumReviewers {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
+		nil, positiveOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	} else {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewer(s) found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
+		nil, negativeOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	}
+	return findings, probeID, nil
+}
 	
-	// returns the number of unique reviewers that aren't the changeset author
-	func uniqueReviewers(authorLogin string, reviewers []string) int {
-		uniqueReviewers := 0
-		for i := range reviewers {
-			duplicateCount := 0
-			for j := range reviewers {
-				if reviewers[j] == reviewers[i] && j > i {
-					duplicateCount++
-				}
-			}
-			if reviewers[i] != authorLogin && duplicateCount == 0 {
-				uniqueReviewers++
+// returns the number of unique reviewers that aren't the changeset author
+func uniqueReviewers(authorLogin string, reviewers []string) int {
+	uniqueReviewers := 0
+	for i := range reviewers {
+		duplicateCount := 0
+		for j := range reviewers {
+			if reviewers[j] == reviewers[i] && j > i {
+				duplicateCount++
 			}
 		}
-		return uniqueReviewers
+		if reviewers[i] != authorLogin && duplicateCount == 0 {
+			uniqueReviewers++
+		}
 	}
+	return uniqueReviewers
+}
