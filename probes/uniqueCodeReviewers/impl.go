8041581db55f7297ce87b68f7c0433c974b1fa82
André Backman <andrebackmann@gmail.com>
2023-07-10 11:20:17 +0300

    update code reviewers implementation; fixed embed FS usage
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/uniqueCodeReviewers/impl.go
+++ probes/uniqueCodeReviewers/impl.go
@@ -2,7 +2,7 @@
 // Licensed under the Apache License 2.0
 // SPDX-License-Identifier: Apache-2.0
 
-package minimumCodeReviewers
+package uniqueCodeReviewers
 
 import (
 	"embed"
@@ -10,16 +10,14 @@ import (
 	"github.com/ossf/scorecard/v4/checker"
 	"github.com/ossf/scorecard/v4/finding"
 	"github.com/ossf/scorecard/v4/probes/utils"
-
-	//"fmt" // extra abackman rm remove
 )
 
+//go:embed *.yml
 var fs embed.FS
 
 const probe = "minimumCodeReviewers"
 
 func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
 	reviewData := raw.CodeReviewResults.DefaultBranchChangesets
-	//fmt.Printf("%v\n\n", reviewData)
 	return utils.CodeReviewRun(reviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
 }
