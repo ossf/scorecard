4d3cafd5568a769fc73c38d0b97aef079f72c624
André Backman <andrebackmann@gmail.com>
2023-07-12 16:15:42 +0300

    refactored uniqueCodeReviewers probe, needs more error checks
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/uniqueCodeReviewers/impl.go
+++ probes/uniqueCodeReviewers/impl.go
@@ -6,7 +6,7 @@ package uniqueCodeReviewers
 
 import (
 	"embed"
-
+	"fmt"
 	"github.com/ossf/scorecard/v4/checker"
 	"github.com/ossf/scorecard/v4/finding"
 	"github.com/ossf/scorecard/v4/probes/utils"
@@ -16,8 +16,58 @@ import (
 var fs embed.FS
 
 const probe = "minimumCodeReviewers"
+const minimumReviewers = 2
 
 func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
-	reviewData := raw.CodeReviewResults.DefaultBranchChangesets
-	return utils.CodeReviewRun(reviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
+	rawReviewData := &raw.CodeReviewResults
+	return CodeReviewRun(rawReviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
+}
+
+
+/*
+** Looks through the data and validates author and reviewers of a changeset
+** Scorecard currently only supports GitHub revisions and generates a positive
+** score in the case of other platforms. This probe is created to ensure that
+** there are a number of unique reviewers for each changeset.
+*/
+
+// reviewData *checker.CodeReviewData
+func CodeReviewRun(reviewData *checker.CodeReviewData, fs embed.FS, probeID string,
+	positiveOutcome, negativeOutcome finding.Outcome,
+	) ([]finding.Finding, string, error) {
+	var findings []finding.Finding
+	leastFoundReviewers := 0
+	changesets := reviewData.DefaultBranchChangesets
+	for i := range changesets {
+		data := &changesets[i]
+		fmt.Printf("\n[DATA] %v", data.Author)
+		if data.ReviewPlatform == "Unknown" && data.Author.Login == "" {
+			continue
+		}
+		reviewersList := make([]string, len(data.Reviews))
+		for i := range data.Reviews {
+			reviewersList[i] = data.Reviews[i].Author.Login
+			fmt.Printf("\n\t[REVIEW] %v", reviewersList[i])
+		}
+		numReviewers := utils.UniqueReviewers(data.Author.Login, reviewersList)
+		if i == 0 || numReviewers < leastFoundReviewers {
+			leastFoundReviewers = numReviewers
+		}
+	}
+	if leastFoundReviewers >= minimumReviewers {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
+		nil, positiveOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	} else {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewer(s) found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
+		nil, negativeOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	}
+	return findings, probeID, nil
 }
--- probes/utils/code_review.go
+++ probes/utils/code_review.go
@@ -3,57 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package utils
-
-import (
-	"embed"
-	"fmt"
-	//"os"
-	"github.com/ossf/scorecard/v4/checker"
-	"github.com/ossf/scorecard/v4/finding"
-)
-
-var minimumReviewers = 2
-
-// Looks through the data and validates author and reviewers of a changeset
-func CodeReviewRun(reviewData []checker.Changeset, fs embed.FS, probeID string,
-	positiveOutcome, negativeOutcome finding.Outcome,
-	) ([]finding.Finding, string, error) {
-	var findings []finding.Finding
-	leastFoundReviewers := 0
-	for i := range reviewData {
-		data := &reviewData[i]
-		if data.ReviewPlatform == "Unknown" && data.Author.Login == "" {
-			continue
-		}
-		reviewersList := make([]string, len(data.Reviews))
-		for i := range data.Reviews {
-			reviewersList[i] = data.Reviews[i].Author.Login
-		}
-		numReviewers := uniqueReviewers(data.Author.Login, reviewersList)
-		if i == 0 || numReviewers < leastFoundReviewers {
-			leastFoundReviewers = numReviewers
-		}
-	}
-	if leastFoundReviewers >= minimumReviewers {
-		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewers found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
-		nil, positiveOutcome)
-		if err != nil {
-			return nil, probeID, fmt.Errorf("create finding: %w", err)
-		}
-		findings = append(findings, *f)
-	} else {
-		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("%v unique reviewer(s) found for at least one changeset, %v wanted.", leastFoundReviewers, minimumReviewers),
-		nil, negativeOutcome)
-		if err != nil {
-			return nil, probeID, fmt.Errorf("create finding: %w", err)
-		}
-		findings = append(findings, *f)
-	}
-	return findings, probeID, nil
-}
 	
 // returns the number of unique reviewers that aren't the changeset author
-func uniqueReviewers(authorLogin string, reviewers []string) int {
+func UniqueReviewers(authorLogin string, reviewers []string) int {
 	uniqueReviewers := 0
 	for i := range reviewers {
 		duplicateCount := 0
