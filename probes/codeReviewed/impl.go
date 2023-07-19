55a1b7953157fd840e37435a179c4ad94f86220a
André Backman <andrebackmann@gmail.com>
2023-07-19 15:54:31 +0300

    codeReviewed probe implemented
    Signed-off-by: André Backman <andre.backman@nokia.com>

---
+++ probes/codeReviewed/impl.go
@@ -0,0 +1,58 @@
+// © 2023 Nokia
+// Licensed under the Apache License 2.0
+// SPDX-License-Identifier: Apache-2.0
+
+package codeReviewed
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+)
+
+//go:embed *.yml
+var fs embed.FS
+
+const probe = "codeReviewed"
+
+func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
+	rawReviewData := &raw.CodeReviewResults
+	return reviewedRun(rawReviewData, fs, probe, finding.OutcomePositive, finding.OutcomeNegative)
+}
+
+/*
+** Looks through the data and validates that each changeset has been approved at least once.
+*/
+
+func reviewedRun(reviewData *checker.CodeReviewData, fs embed.FS, probeID string,
+	positiveOutcome, negativeOutcome finding.Outcome,
+) ([]finding.Finding, string, error) {
+	var findings []finding.Finding
+	var numReviews = 0
+	changesets := reviewData.DefaultBranchChangesets
+	var numChangesets = len(changesets)
+	for x := range changesets {
+		data := &changesets[x]
+		if len(data.Reviews) > 0 {
+			numReviews += 1
+		}
+	}
+	if numReviews >= numChangesets {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("All changesets have review activity (%v out of %v).", numReviews, numChangesets),
+			nil, positiveOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	} else {
+		f, err := finding.NewWith(fs, probeID, fmt.Sprintf("Not all changesets have review activity. Found %v reviews among %v changesets.", numReviews, numChangesets),
+			nil, negativeOutcome)
+		if err != nil {
+			return nil, probeID, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	}
+	return findings, probeID, nil
+}
