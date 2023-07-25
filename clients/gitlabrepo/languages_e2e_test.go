3517fb98b7d01910b8f0d8bba3ef48ec44446dd0
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2023-07-25 07:38:33 -0500

    :seedling: Improve `gitlabrepo.ListProgrammingLanguages` e2e tests
    - Add an end-to-end test for the `gitlabrepo.ListProgrammingLanguages` function
    - Include a test for repos without code to check that no programming languages are returned
    
    [clients/gitlabrepo/languages_e2e_test.go]
    - Add an e2e test for the `gitlabrepo.ListProgrammingLanguages` function
    - Add a test for repos without code to check that no programming languages are returned
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

---
+++ clients/gitlabrepo/languages_e2e_test.go
@@ -0,0 +1,52 @@
+// Copyright 2023 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gitlabrepo
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("E2E TEST: gitlabrepo.ListProgrammingLanguages", func() {
+	Context("Test list programming languages - GitLab", func() {
+		It("returns branches for the repo", func() {
+			repo, err := MakeGitlabRepo("https://gitlab.com/baserow/baserow")
+			Expect(err).Should(BeNil())
+
+			client, err := CreateGitlabClient(context.Background(), repo.Host())
+			Expect(err).Should(BeNil())
+
+			err = client.InitRepo(repo, "HEAD", 0)
+			Expect(err).Should(BeNil())
+			programmingLang, err := client.ListProgrammingLanguages()
+			Expect(err).Should(BeNil())
+			Expect(programmingLang).ShouldNot(BeNil())
+		})
+		It("Should return no programming languages repo with no code", func() {
+			repo, err := MakeGitlabRepo("https://gitlab.com/ossf-test/scorecard-test-branches")
+			Expect(err).Should(BeNil())
+
+			client, err := CreateGitlabClient(context.Background(), repo.Host())
+			Expect(err).Should(BeNil())
+
+			err = client.InitRepo(repo, "HEAD", 0)
+			Expect(err).Should(BeNil())
+			programmingLang, err := client.ListProgrammingLanguages()
+			Expect(err).Should(BeNil())
+			Expect(programmingLang).Should(BeNil())
+		})
+	})
+})
