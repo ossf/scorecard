b992be36e0bdfc0f202f737b9fbe7f1afb966d10
Pete Wagner <1559510+thepwagner@users.noreply.github.com>
2023-07-28 13:05:32 -0400

    :seedling: Bump github.com/google/go-github from v38.1.0 to v53.2.0 (#3327)
    Signed-off-by: Peter Wagner <peter.wagner@shopify.com>

--- clients/githubrepo/webhook_test.go
+++ clients/githubrepo/webhook_test.go
@@ -21,7 +21,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v38/github"
+	"github.com/google/go-github/v53/github"
 
 	"github.com/ossf/scorecard/v4/clients"
 )
