1e944ddb0ba98862ea309fd0770a409ddc93696b
André Backman <andre.backman@nokia.com>
2023-08-02 14:22:48 +0300

    fix linting error
    Signed-off-by: André Backman <andre.backman@nokia.com>

--- probes/codeReviewTwoReviewers/impl.go
+++ probes/codeReviewTwoReviewers/impl.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// nolint
+//nolint
 package codeReviewTwoReviewers
 
 import (
@@ -123,4 +123,3 @@ func uniqueReviewers(changesetAuthor string, reviews []clients.Review) (int, err
 	}
 	return len(reviewersList), nil
 }
-
