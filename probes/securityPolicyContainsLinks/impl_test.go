a8b255a224c32ca2ea75c57ba414e6c6cc6241b3
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2023-08-03 21:52:15 -0700

    ✨ [experimental] Probe support for security policy check (#3241)
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * fix unit tests
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * comments
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * compilation fix
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * missing file
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * missing file
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update reason string
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * typo
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * fix unit tests
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * typo
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * unit tests and linnter
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * comments
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * comments
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * missing file
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * unit tests for probes
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * linter
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * revert FileSize change
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    ---------
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>

---
+++ probes/securityPolicyContainsLinks/impl_test.go
@@ -0,0 +1,308 @@
+// Copyright 2023 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// nolint:stylecheck
+package securityPolicyContainsLinks
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+	"github.com/ossf/scorecard/v4/probes/internal/utils"
+)
+
+func Test_Run(t *testing.T) {
+	t.Parallel()
+	// nolint:govet
+	tests := []struct {
+		name     string
+		raw      *checker.RawResults
+		outcomes []finding.Outcome
+		err      error
+	}{
+		{
+			name: "file present on repo no link",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "file present on repo link",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeLink,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "https://www.bla.com",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+			},
+		},
+		{
+			name: "file present on repo email",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeEmail,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "hey@google.com",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+			},
+		},
+		{
+			name: "file present on org no link",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "file present on org link",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeURL,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeLink,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "https://www.bla.com",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+			},
+		},
+		{
+			name: "file present on org email",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeURL,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeEmail,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "hey@google.com",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+			},
+		},
+		{
+			name: "files present on org and repo",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeURL,
+							},
+						},
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomeNegative,
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "files present on org and repo email",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeURL,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeEmail,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "hey@google.com",
+									},
+								},
+							},
+						},
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "files present on org and repo link",
+			raw: &checker.RawResults{
+				SecurityPolicyResults: checker.SecurityPolicyData{
+					PolicyFiles: []checker.SecurityPolicyFile{
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeURL,
+							},
+							Information: []checker.SecurityPolicyInformation{
+								{
+									InformationType: checker.SecurityPolicyInformationTypeLink,
+									InformationValue: checker.SecurityPolicyValueType{
+										Match: "https://www.bla.com",
+									},
+								},
+							},
+						},
+						{
+							File: checker.File{
+								Path: "SECURITY.md",
+								Type: finding.FileTypeText,
+							},
+						},
+					},
+				},
+			},
+			outcomes: []finding.Outcome{
+				finding.OutcomePositive,
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "file not present",
+			raw:  &checker.RawResults{},
+			outcomes: []finding.Outcome{
+				finding.OutcomeNegative,
+			},
+		},
+		{
+			name: "nil raw",
+			err:  utils.ErrNil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt // Re-initializing variable so it is not changed while executing the closure below
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			findings, s, err := Run(tt.raw)
+			if !cmp.Equal(tt.err, err, cmpopts.EquateErrors()) {
+				t.Errorf("mismatch (-want +got):\n%s", cmp.Diff(tt.err, err, cmpopts.EquateErrors()))
+			}
+			if err != nil {
+				return
+			}
+			if diff := cmp.Diff(Probe, s); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(len(tt.outcomes), len(findings)); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+			for i := range tt.outcomes {
+				outcome := &tt.outcomes[i]
+				f := &findings[i]
+				if diff := cmp.Diff(*outcome, f.Outcome); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+			}
+		})
+	}
+}
---
+++ probes/securityPolicyContainsText/impl.go
@@ -0,0 +1,75 @@
+// Copyright 2023 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// nolint:stylecheck
+package securityPolicyContainsText
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/ossf/scorecard/v4/checker"
+	"github.com/ossf/scorecard/v4/finding"
+	"github.com/ossf/scorecard/v4/probes/internal/utils"
+)
+
+//go:embed *.yml
+var fs embed.FS
+
+const Probe = "securityPolicyContainsText"
+
+func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
+	if raw == nil {
+		return nil, "", fmt.Errorf("%w: raw", utils.ErrNil)
+	}
+	var findings []finding.Finding
+	policies := raw.SecurityPolicyResults.PolicyFiles
+	for i := range policies {
+		policy := &policies[i]
+		linkedContentLen := 0
+		emails := utils.CountSecInfo(policy.Information, checker.SecurityPolicyInformationTypeEmail, true)
+		urls := utils.CountSecInfo(policy.Information, checker.SecurityPolicyInformationTypeLink, true)
+		for _, i := range utils.FindSecInfo(policy.Information, checker.SecurityPolicyInformationTypeEmail, true) {
+			linkedContentLen += len(i.InformationValue.Match)
+		}
+		for _, i := range utils.FindSecInfo(policy.Information, checker.SecurityPolicyInformationTypeLink, true) {
+			linkedContentLen += len(i.InformationValue.Match)
+		}
+
+		if policy.File.FileSize > 1 && (policy.File.FileSize > uint(linkedContentLen+((urls+emails)*2))) {
+			f, err := finding.NewPositive(fs, Probe,
+				"Found text in security policy", policy.File.Location())
+			if err != nil {
+				return nil, Probe, fmt.Errorf("create finding: %w", err)
+			}
+			findings = append(findings, *f)
+		} else {
+			f, err := finding.NewNegative(fs, Probe,
+				"No text (besides links / emails) found in security policy", nil)
+			if err != nil {
+				return nil, Probe, fmt.Errorf("create finding: %w", err)
+			}
+			findings = append(findings, *f)
+		}
+	}
+
+	if len(findings) == 0 {
+		f, err := finding.NewNegative(fs, Probe, "no security file to analyze", nil)
+		if err != nil {
+			return nil, Probe, fmt.Errorf("create finding: %w", err)
+		}
+		findings = append(findings, *f)
+	}
+	return findings, Probe, nil
+}
