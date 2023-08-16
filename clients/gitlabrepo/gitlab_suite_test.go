5a20bdceff8a83e94bdc3f10c3bbac9c9736c386
Spencer Schrock <sschrock@google.com>
2023-08-16 16:09:11 -0700

    Move ginkgo setup suite to own file.
    The other e2e suites do this already.
    
    Signed-off-by: Spencer Schrock <sschrock@google.com>

---
+++ clients/gitlabrepo/gitlab_suite_test.go
@@ -0,0 +1,32 @@
+// Copyright 2023 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gitlabrepo
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGitlabRepoE2E(t *testing.T) {
+	if val, exists := os.LookupEnv("SKIP_GINKGO"); exists && val == "1" {
+		t.Skip()
+	}
+	t.Parallel()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GitLab Repo Suite")
+}
