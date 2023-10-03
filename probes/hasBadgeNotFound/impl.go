f55d89b9a5ac79553bfc44abb38afccb28f0c423
AdamKorcz <adam@adalogics.com>
2023-10-03 16:10:45 +0100

    Edit text in def.yml
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- probes/hasBadgeNotFound/impl.go
+++ probes/hasBadgeNotFound/impl.go
@@ -45,7 +45,7 @@ func Run(raw *checker.RawResults) ([]finding.Finding, string, error) {
 
 func negativeOutcome() ([]finding.Finding, string, error) {
 	f, err := finding.NewWith(fs, Probe,
-		"No OpenSSF badge found.", nil,
+		"OpenSSF best practice badge not found", nil,
 		finding.OutcomeNegative)
 	if err != nil {
 		return nil, Probe, fmt.Errorf("create finding: %w", err)
@@ -55,7 +55,7 @@ func negativeOutcome() ([]finding.Finding, string, error) {
 
 func positiveOutcome() ([]finding.Finding, string, error) {
 	f, err := finding.NewWith(fs, Probe,
-		"OpenSSF badge found.", nil,
+		"OpenSSF best practice badge found.", nil,
 		finding.OutcomePositive)
 	if err != nil {
 		return nil, Probe, fmt.Errorf("create finding: %w", err)
