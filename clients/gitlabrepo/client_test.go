3b347180f88ca5ce267b8b61163d407cdc11dabd
Allen Shearin <allen.p.shearin@gmail.com>
2024-01-03 13:15:07 -0700

    fix: gitlab listcommits tests, remove else in commit array length check
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>

--- clients/gitlabrepo/client_test.go
+++ clients/gitlabrepo/client_test.go
@@ -15,11 +15,25 @@
 package gitlabrepo
 
 import (
+	"context"
 	"errors"
+	"net/http"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/xanzy/go-gitlab"
+
+	"github.com/ossf/scorecard/v4/clients"
 )
+
+type goodGatewayRoundTripper struct{}
+
+func (g goodGatewayRoundTripper) RoundTrip(*http.Request) (*http.Response, error) {
+	return &http.Response{
+		Status:     "200 OK",
+		StatusCode: http.StatusOK,
+	}, nil
+}
 
 func TestCheckRepoInaccessible(t *testing.T) {
 	t.Parallel()
@@ -65,6 +79,82 @@ func TestCheckRepoInaccessible(t *testing.T) {
 			got := checkRepoInaccessible(tt.repo)
 			if !errors.Is(got, tt.want) {
 				t.Errorf("checkRepoInaccessible() got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestListCommits(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		responsePath string
+		commits      []clients.Commit
+		wantErr      bool
+	}{
+		{
+			name:         "Error in ListRawCommits",
+			responsePath: "./testdata/invalid-commits",
+			commits:      []clients.Commit{},
+			wantErr:      true,
+		},
+		{
+			name:         "No commits in repo",
+			responsePath: "./testdata/valid-checkruns-1", // Contains Empty Array
+			commits:      []clients.Commit{},
+			wantErr:      false,
+		},
+		{
+			name:         "Error in getMergeRequestDetail",
+			responsePath: "./testdata/valid-commits",
+			commits:      []clients.Commit{},
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			httpClient := &http.Client{
+				Transport: suffixStubTripper{
+					responsePaths: map[string]string{
+						"commits": tt.responsePath, // corresponds to projects/<id>/repository/commits
+					},
+				},
+			}
+			glclient, err := gitlab.NewClient("", gitlab.WithHTTPClient(httpClient))
+			if err != nil {
+				t.Fatalf("gitlab.NewClient error: %v", err)
+			}
+			commitshandler := &commitsHandler{
+				glClient: glclient,
+			}
+
+			repoURL := repoURL{
+				owner:     "ossf-tests",
+				commitSHA: clients.HeadSHA,
+			}
+
+			commitshandler.init(&repoURL, 30)
+
+			gqlhandler := graphqlHandler{
+				client: &http.Client{
+					Transport: goodGatewayRoundTripper{},
+				},
+			}
+			gqlhandler.init(context.Background(), &repoURL)
+
+			client := &Client{glClient: glclient, commits: commitshandler, graphql: &gqlhandler}
+
+			got, Err := client.ListCommits()
+
+			if (Err != nil) != tt.wantErr {
+				t.Fatalf("ListCommits, wanted Error: %v, got Error: %v", tt.wantErr, Err)
+			}
+			if !cmp.Equal(got, tt.commits) {
+				t.Errorf("ListCommits() got %v, want %v", got, tt.commits)
 			}
 		})
 	}
