f9dc606cf908535d8e4de6b4f0e321b347d3f91f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-01-17 21:23:08 -0500

    spelling: package
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- checks/raw/shell_download_validate.go
+++ checks/raw/shell_download_validate.go
@@ -805,7 +805,7 @@ func isNugetUnpinnedDownload(cmd []string) bool {
 	return false
 }
 
-func collectUnpinnedPakageManagerDownload(startLine, endLine uint, node syntax.Node,
+func collectUnpinnedPackageManagerDownload(startLine, endLine uint, node syntax.Node,
 	cmd, pathfn string, r *checker.PinningDependenciesData,
 ) {
 	ce, ok := node.(*syntax.CallExpr)
@@ -1146,7 +1146,7 @@ func validateShellFileAndRecord(pathfn string, startLine, endLine uint, content 
 		collectFetchProcSubsExecute(startLine, endLine, node, cmdStr, pathfn, r)
 
 		// Package manager's unpinned installs.
-		collectUnpinnedPakageManagerDownload(startLine, endLine, node, cmdStr, pathfn, r)
+		collectUnpinnedPackageManagerDownload(startLine, endLine, node, cmdStr, pathfn, r)
 
 		// TODO(laurent): add check for cat file | bash.
 		// TODO(laurent): detect downloads of zip/tar files containing scripts.
