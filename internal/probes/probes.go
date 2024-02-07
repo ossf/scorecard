e6dd4a2846337a6eae7c1c25e38c71add31c7149
Spencer Schrock <sschrock@google.com>
2024-02-07 15:35:59 -0800

    redefine probeimpl to avoid circular imports
    Signed-off-by: Spencer Schrock <sschrock@google.com>

--- internal/probes/probes.go
+++ internal/probes/probes.go
@@ -1,15 +1,18 @@
 package probes
 
 import (
+	"github.com/ossf/scorecard/v4/checker"
 	"github.com/ossf/scorecard/v4/errors"
-	"github.com/ossf/scorecard/v4/probes"
+	"github.com/ossf/scorecard/v4/finding"
 )
 
+type ProbeImpl func(*checker.RawResults) ([]finding.Finding, string, error)
+
 // registered is the mapping of all registered probes.
-var registered = map[string]probes.ProbeImpl{}
+var registered = map[string]ProbeImpl{}
 
 // TODO: handle raw data and support types
-func Register(name string, implementation probes.ProbeImpl) error {
+func Register(name string, implementation ProbeImpl) error {
 	if name == "" {
 		//nolint:wrapcheck // fix this in config later
 		return errors.CreateInternal(errors.ErrScorecardInternal, "name cannot be empty")
