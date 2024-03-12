696b76bf4c6a6b8a5b444654b7faaaa551e2dacf
Gabriela Gutierrez <gabigutierrez@google.com>
2024-03-12 17:10:08 +0000

    refactor: Separate annotations content from config parsing
    Signed-off-by: Gabriela Gutierrez <gabigutierrez@google.com>

--- config/config.go
+++ config/config.go
@@ -24,45 +24,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// Reason is the reason behind an annotation.
-type Reason string
-
-const (
-	// TestData is to annotate when a check or probe is targeting a danger
-	// in files or code snippets only used for test or example purposes.
-	TestData Reason = "test-data"
-	// Remediated is to annotate when a check or probe correctly identified a
-	// danger and, even though the danger is necessary, a remediation was already applied.
-	// E.g. a workflow is dangerous but only run under maintainers verification and approval,
-	// or a binary is needed but it is signed or has provenance.
-	Remediated Reason = "remediated"
-	// NotApplicable is to annotate when a check or probe is not applicable for the case.
-	// E.g. the dependencies should not be pinned because the project is a library.
-	NotApplicable Reason = "not-applicable"
-	// NotSupported is to annotate when the maintainer fulfills a check or probe in a way
-	// that is not supported by Scorecard. E.g. Clang-Tidy is used as SAST tool but not identified
-	// because its not supported.
-	NotSupported Reason = "not-supported"
-	// NotDetected is to annotate when the maintainer fulfills a check or probe in a way
-	// that is supported by Scorecard but not identified. E.g. Dependabot is configured in the
-	// repository settings and not in a file.
-	NotDetected Reason = "not-detected"
-)
-
-// ReasonGroup groups the annotation reason and, in the future, the related probe.
-// If there is a probe, the reason applies to the probe.
-// If there is not a probe, the reason applies to the check or checks in
-// the group.
-type ReasonGroup struct {
-	Reason Reason `yaml:"reason"`
-}
-
-// Annotation defines a group of checks that are being annotated for various reasons.
-type Annotation struct {
-	Checks  []string      `yaml:"checks"`
-	Reasons []ReasonGroup `yaml:"reasons"`
-}
-
 // Config contains configurations defined by maintainers.
 type Config struct {
 	Annotations []Annotation `yaml:"annotations"`
@@ -98,22 +59,4 @@ func Parse(repoClient clients.RepoClient) (Config, error) {
 
 	// Return configuration
 	return c, nil
-}
-
-// Doc maps a reason to its human-readable explanation.
-func (r *Reason) Doc() string {
-	switch *r {
-	case TestData:
-		return "The files or code snippets are only used for test or example purposes."
-	case Remediated:
-		return "The dangerous files or code snippets are necessary but remediations were already applied."
-	case NotApplicable:
-		return "The check or probe is not applicable in this case."
-	case NotSupported:
-		return "The check or probe is fulfilled but in a way that is not supported by Scorecard."
-	case NotDetected:
-		return "The check or probe is fulfilled but in a way that is supported by Scorecard but it was not detected."
-	default:
-		return string(*r)
-	}
 }
