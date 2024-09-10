5ca93001b2108c53c89f33fdf0dd6f09963d386f
6543 <6543@obermui.de>
2024-09-11 00:16:17 +0200

    Update probes/testsRunInCI/impl.go
    Signed-off-by: 6543 <6543@obermui.de>

--- probes/testsRunInCI/impl.go
+++ probes/testsRunInCI/impl.go
@@ -165,7 +165,7 @@ func isTest(s string) bool {
 	for _, pattern := range []string{
 		"appveyor", "buildkite", "circleci", "e2e", "github-actions", "jenkins",
 		"mergeable", "packit-as-a-service", "semaphoreci", "test", "travis-ci",
-		"flutter-dashboard", "Cirrus CI", "azure-pipelines", "woodpecker",
+		"flutter-dashboard", "Cirrus CI", "azure-pipelines", "ci/woodpecker",
 	} {
 		if strings.Contains(l, pattern) {
 			return true
