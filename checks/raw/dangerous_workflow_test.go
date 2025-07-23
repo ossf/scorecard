5d9fe3760e8ce8b9495d257cfac0107bf60e8568
Adam Korczynski <adam@adalogics.com>
2025-07-23 21:23:02 +0100

    🐛 detect label names in dangerous workflows
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- checks/raw/dangerous_workflow_test.go
+++ checks/raw/dangerous_workflow_test.go
@@ -86,6 +86,16 @@ func TestUntrustedContextVariables(t *testing.T) {
 			variable: "github.event.commits[2].author.email",
 			expected: true,
 		},
+		{
+			name:     "PR label name",
+			variable: "github.event.pull_request.labels.foo.name",
+			expected: true,
+		},
+		{
+			name:     "PR label wildcard name",
+			variable: "github.event.pull_request.labels.*.name",
+			expected: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
