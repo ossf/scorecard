ce9d7e6162c443b87eb81e9f427e883c1d37b736
Evan Anderson <evan.k.anderson@gmail.com>
2025-07-29 10:19:10 -0700

    Add license header
    Signed-off-by: Evan Anderson <evan.k.anderson@gmail.com>

--- checks/minder.go
+++ checks/minder.go
@@ -1,3 +1,17 @@
+// Copyright 2025 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package checks
 
 import (
@@ -13,6 +27,9 @@ func init() {
 
 	for _, rule := range minder.AllRules {
 		rulefunc := minder.CheckRule(rule)
-		registerCheck(rule.Name, rulefunc, IngestToType[rule.Def.Ingest.Type])
+		err := registerCheck(rule.Name, rulefunc, IngestToType[rule.Def.Ingest.Type])
+		if err != nil {
+			panic("Failed to register minder rule: " + rule.Name + ": " + err.Error())
+		}
 	}
 }
--- checks/minder/file_git_adapter.go
+++ checks/minder/file_git_adapter.go
@@ -1,3 +1,17 @@
+// Copyright 2025 OpenSSF Scorecard Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package minder
 
 import (
@@ -5,6 +19,7 @@ import (
 	billyutil "github.com/go-git/go-billy/v5/util"
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
+
 	"github.com/ossf/scorecard/v5/checks/fileparser"
 	"github.com/ossf/scorecard/v5/clients"
 )
