954b5f7f2bb96379524ecec0d65d609506288dff
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2025-09-10 23:31:06 +0100

    :bug: do not include asName when creating digest of FROM name as asName (#4683)
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- checks/raw/pinned_dependencies_test.go
+++ checks/raw/pinned_dependencies_test.go
@@ -378,6 +378,11 @@ func TestDockerfilePinning(t *testing.T) {
 			warns:    1,
 		},
 		{
+			name:     "Non-pinned dockerfile with as - simple",
+			filename: "Dockerfile-not-pinned-as-simple",
+			warns:    1,
+		},
+		{
 			name:     "Parser error doesn't affect docker image pinning",
 			filename: "Dockerfile-not-pinned-with-parser-error",
 			warns:    1,
@@ -1939,6 +1944,33 @@ func TestCollectDockerfilePinning(t *testing.T) {
 						Snippet:   "FROM python:3.7",
 						Offset:    17,
 						EndOffset: 17,
+						FileSize:  0,
+						Type:      1,
+					},
+					Pinned: boolAsPointer(false),
+					Type:   "containerImage",
+					Remediation: &finding.Remediation{
+						Text: "pin your Docker image by updating python:3.7 to python:3.7" +
+							"@sha256:eedf63967cdb57d8214db38ce21f105003ed4e4d0358f02bedc057341bcf92a0",
+						Markdown: "pin your Docker image by updating python:3.7 to python:3.7" +
+							"@sha256:eedf63967cdb57d8214db38ce21f105003ed4e4d0358f02bedc057341bcf92a0",
+					},
+				},
+			},
+		},
+		{
+			name:        "Non-pinned dockerfile as simple",
+			filename:    "./testdata/Dockerfile-not-pinned-as-simple",
+			expectError: false,
+			outcomeDependencies: []checker.Dependency{
+				{
+					Name:     stringAsPointer("python"),
+					PinnedAt: stringAsPointer("3.7"),
+					Location: &checker.File{
+						Path:      "./testdata/Dockerfile-not-pinned-as-simple",
+						Snippet:   "FROM python:3.7 as build",
+						Offset:    16,
+						EndOffset: 16,
 						FileSize:  0,
 						Type:      1,
 					},
